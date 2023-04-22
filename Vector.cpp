82f816d3ed9e79b46614884df18e8a93032e9883
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-04-23 00:06:16 +0530

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -4,4 +4,32 @@ using namespace std;
 int main()
 {
    
+int main() {
+    vector<int> v;
+    
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    
+    for(int i=0; i<v.size(); i++) {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    
+    v.pop_back();
+    v.insert(v.begin()+1, 4);
+    v[2] = 5;
+    
+    for(auto x : v) {
+        cout << x << " ";
+    }
+    cout << endl;
+    
+    v.clear();
+    
+    cout << "Size of vector: " << v.size() << endl;
+    
+    return 0;
+}
+
 }
