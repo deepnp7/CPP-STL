665220be207509508dc6ce6bf1a55ce631961643
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-04-24 18:18:53 +0530

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -9,7 +9,8 @@ int main()
     v.push_back(2);
     v.push_back(3);
     
-    for(int i=0; i<v.size(); i++) {
+    for(int i=0; i<v.size(); i++) 
+    {
         cout << v[i] << " ";
     }
     cout << endl;
