6fd5e08054d40cac3529d14511d1fbe9829b1f1b
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-04-29 15:41:59 +0530

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -1,36 +1,47 @@
-#include<iostream>
-#include<vector>    //header 
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() 
+void printVector(vector<int>v)
 {
-    vector<int> v;
-    
-    v.push_back(1);
-    v.push_back(2);
-    v.push_back(3);
-    
-    for(int i=0; i<v.size(); i++) 
-    {
-     cout << v[i] << " ";
-    }
-    cout << endl;
-    
-    v.pop_back();
-    v.insert(v.begin()+1, 4);
-    v[2] = 5;
-    
-    for(auto x : v) 
-    {
-        cout << x << " ";
-    }
-    cout << endl;
-  
-    v.clear(); // clearing the
-    
-    cout << "Size of vector: " << v.size() << endl;
-    
-    return 0;
+    int size =v.size();
+    cout<<"Size : "<<v.size()<<endl;
+    cout<<"The elements are : ";
+   /*
+      for (int i = 0; i < size; i++)\
+      {
+       cout<<v[i]<<" ";        // Iterating through a vector 
+      }
+   */ 
+       for(auto it : v)
+       {
+        cout<<it<<" ";       // Iterating through a vector
+       }
+    cout<<endl;
 }
 
+int main()
+{
+ vector<int>v;
+ v.push_back(1);     // adding elements to the vector
+ v.push_back(2);
+ v.push_back(3);
+ v.push_back(4);
+ v.push_back(5);
+ v.push_back(6);
+ printVector(v);   // printing the vector after pushing the elements to the vector
+ cout<<endl;
 
+ cout<<"After the Operations "<<endl<<endl;
+
+ v.insert(v.begin(),8);     // inserting an element at 1st index
+ v.insert(v.begin()+2,7);  // inserting an element at 2nd index
+ v.pop_back();            //removing the last element from the vector
+ v.erase(v.begin()+3);   // removing the element from 3rd index
+ int a=v.at(2);         // printing the element at 2nd index
+ v.at(3)=9;            // modifying the 3rd element to 9
+ int b=v.back();      // printing the last element of the vector
+ cout<<"Element at 2nd index: "<<a<<endl<<"Last element: "<<b<<endl;
+ 
+  printVector(v);  // printing the vector after performing different operations
+  return 0;
+} 
