6e452bcb97cdfe115ed61f56e090e94844479f91
Deepnarayan Patra <105645365+deepnp7@users.noreply.github.com>
2023-05-05 18:23:49 +0530

    Update Stack.cpp

--- Stack.cpp
+++ Stack.cpp
@@ -1,7 +1,38 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <stack>
 using namespace std;
-int main()
-{
-  
-}
 
+int main() {
+    stack<int> s; // create an empty stack of integers
+    
+    // push elements into the stack
+    s.push(10);
+    s.push(20);
+    s.push(30);
+
+    // print the top element
+    cout << "Top element: " << s.top() << endl;
+
+    // pop the top element
+    s.pop();
+
+    // print the size of the stack
+    cout << "Size of stack: " << s.size() << endl;
+
+    // check if stack is empty
+    if (s.empty()) {
+        cout << "Stack is empty" << endl;
+    } else {
+        cout << "Stack is not empty" << endl;
+    }
+
+    // print all elements of the stack
+    cout << "Elements in the stack: ";
+    while (!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
